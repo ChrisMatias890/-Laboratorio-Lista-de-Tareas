7a7f1739dea1e0273f0c309f6a6050071a1013b5
Matias <christian.matias@dewey.edu>
2025-03-28 11:59:22 -0400

    Finalizado
    Este commit es para finalizar completamente la tarea.

--- listatareas.rb
+++ listatareas.rb
@@ -19,28 +19,48 @@ class TaskList
     #Método para agregar tareas:
     def agregar_tareas()
         begin
-            #Preguntando al usuario cuantas tareas deseas agregar:
-            print("¿Cuántas tareas deseas agregar?: ")
-            cantidad_tareas = gets.chomp.to_i
+            loop do
+                #Preguntando al usuario cuantas tareas deseas agregar:
+                print("¿Cuántas tareas deseas agregar?: ")
+                cantidad_tareas = gets.chomp.to_i
+                
+                if cantidad_tareas == " "
+                    puts("\nValor no ingresado.")
+                    puts("\nPor favor, ingréselo de nuevo.")
+                elsif cantidad_tareas <= 0
+                    puts("\nIngresaste un valor menor incorrecto.")
+                    puts("\nPor favor, ingréselo de nuevo.")
+                else
+                        cantidad_tareas.times do |i|
+                            loop do
+                                #Variable para las fechas:
+                                fecha = Time.now.strftime("%m/%d/%Y %H:%M:%S")
+                                print("Ingrese el nombre de la tarea #{i + 1}: ")
+                                nombre = gets.chomp.capitalize
 
-            cantidad_tareas.times do |i|
-                #Variable para las fechas:
-                fecha = Time.now.strftime("%m/%d/%Y %H:%M:%S")
-                print("Ingrese el nombre de la tarea #{i + 1}: ")
-                nombre = gets.chomp.capitalize
-
-                print("Ingrese la descripción de la tarea: ")
-                descripcion = gets.chomp.capitalize
-
-                @listatareas[nombre] = {
-                descripcion: descripcion,
-                estado: false,
-                creada_en: fecha, 
-                finalizada_en: nil,
-                }
-
-                puts("La tarea #{nombre} fue creada exitosamente.")
-            end # Final del ciclo for
+                                print("Ingrese la descripción de la tarea #{i + 1}: ")
+                                descripcion = gets.chomp.capitalize
+                                if nombre.empty? || descripcion.empty?
+                                    puts("No se pueden dejar vacías el nombre y las descripciones, por favor. inténtelo de nuevo.")
+                                else
+                                @listatareas[nombre] = {
+                                descripcion: descripcion,
+                                estado: false,
+                                creada_en: fecha, 
+                                finalizada_en: nil,
+                                }
+                                break
+                                end # Final de la condición 
+                                
+                                puts("La tarea #{nombre} fue creada exitosamente.")
+                            end #Final del loop
+                           
+                            
+                           
+                        end # Final del ciclo do
+                    break
+                end #fin de la condicion
+            end #Fin del ciclo loop
         rescue StandardError => e
             puts("Hubo un error al intentar crear la tarea, erorr: #{e.message}")
         end #Final del manejo de excepciones
@@ -57,7 +77,7 @@ class TaskList
                 #Creando un loop con el método .each_with_index para que me ponga un de forma númerica las tareas.
                 @listatareas.each_with_index do |(nombre, tarea), index|
                 estado = tarea[:estado] ? "Completada" : "Pendiente"
-                puts "#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} (Creada: #{tarea[:creada_en]})"
+                puts "#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} - (Creada: #{tarea[:creada_en]})"
                 end # Fin del ciclo do 
 
                 loop do
@@ -70,7 +90,7 @@ class TaskList
                     if @listatareas[nombre]
                         fecha = Time.now.strftime("%m/%d/%Y %H:%M:%S")
                         @listatareas[nombre][:estado] = true
-                        @listatareas[nombre][:finalizada] = fecha
+                        @listatareas[nombre][:finalizada_en] = fecha
                         puts("\n La tarea #{nombre} fue completada con éxito.")
                         break
                     else
@@ -94,10 +114,10 @@ class TaskList
                 #Creando un loop con el método .each_with_index para que me ponga un de forma númerica las tareas.
                 @listatareas.each_with_index do |(nombre, tarea), index|
                     estado = tarea[:estado] ? "Completada" : "Pendiente"
-                    if tarea[:estado] = "Completada"
-                        puts ("#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} (Creada: #{tarea[:creada_en]}, (Finalizada: #{tarea[:finalizada]})")
+                    if tarea[:estado] == true
+                        puts ("#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} - (Creada: #{tarea[:creada_en]}, (Finalizada: #{tarea[:finalizada_en]})")
                     else
-                        puts ("#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} (Creada: #{tarea[:creada_en]})")
+                        puts ("#{index + 1}. #{nombre}: #{tarea[:descripcion]} - Estado: #{estado} - (Creada: #{tarea[:creada_en]})")
                     end #fin de la condicional
                 end # Fin del ciclo do
             end # Final de la condicion    
@@ -151,79 +171,67 @@ class TaskList
         end #Fin del manejo de excepciones
     end #Final de eliminar tareas
         
-    def guardar_tarea()
+    #Función para guardar tareas
+    def guardar_tareas()
         begin
             if @listatareas.empty?
-                puts("No hay tareas registradas")
+                puts ("No hay tareas registradas.")
             else
-                print("\n Ingrese el nombre del archivo: ")
+                print ("Ingrese el nombre del archivo (sin formato txt): ")
                 archivo = gets.chomp.downcase
-                txt = ".txt"
-
-                nombre_archivo = archivo + txt
-                if File.exist?(nombre_archivo)
-                    print("El archivo  ya fue existe")
-                else
-                    File.open(nombre_archivo, "w") do |file|
-                        @listatareas.each do |nombre, tarea|
-                            file.puts("| #{nombre} | #{tarea[:descripcion]} | #{tarea[:estado]}, | #{tarea[:creada_en]} | #{tarea[:finalizada_en]} |")
-                        end #Final del ciclo do
-                    end #Final del otro ciclo do
-                    
-                end #Final de la condicion
-            end # Fin de la condicion
+                nombre_archivo = "#{archivo}.txt"
+            
+                File.open(nombre_archivo, "w") do |file|
+                @listatareas.each do |nombre, tarea|
+                    file.puts("#{nombre}|#{tarea[:descripcion]}|#{tarea[:estado]}|#{tarea[:creada_en]}|#{tarea[:finalizada_en]}")
+                end # Fin del ciclo do para abrir archivo
+                end # Fin del ciclo do para escribir en el archivo
+                puts ("Las tareas se han guardado correctamente en '#{nombre_archivo}'." )
+            end #Fin de la condicional
         rescue StandardError => e
             puts("Hubo un error al intentar guardar las tareas, error: #{e.message}")
-        end #Fin del manejo de excepciones        
-    end # Fin de la funcion guardar_tarea
+        end #Fin del manejo de excepciones
+      end #Fin de la función guardar tareas
         
-    def cargar_tareas()
+      #Función para cargar tareas en el archivo
+      def cargar_tareas()
         begin
-          print("\n Ingrese el nombre del archivo: ")
-          archivo = gets.chomp.downcase
-          txt = ".txt"
-      
-          nombre_archivo = archivo + txt
-          if File.exist?(nombre_archivo)
-            @listatareas = {}
-      
-            # Abre el archivo y lee línea por línea
+            print "Ingrese el nombre del archivo (sin formato txt): "
+            archivo = gets.chomp.downcase
+            nombre_archivo = "#{archivo}.txt"
+        
+            unless File.exist?(nombre_archivo)
+            puts ("El archivo '#{nombre_archivo}' no existe.")
+            end #Fin de la condicional
+        
+            @listatareas = {} # Inicializar el diccionario
             File.readlines(nombre_archivo).each do |linea|
-              # Dividir la línea en partes
-              nombre, descripcion, estado, creada_en, finalizada_en = linea.chomp.split('|')
-      
-              # Convertir las fechas usando Time.parse y manejar posibles errores
-              begin
-                fecha_creada = Time.strptime(creada_en, "%m/%d/%Y %H:%M:%S")
-                fecha_finalizada = finalizada_en.nil? || finalizada_en.strip.empty? ? nil : Time.strptime(finalizada_en, "%m/%d/%Y %H:%M:%S")
-              rescue ArgumentError
-                puts "Error al cargar las fechas en la tarea '#{nombre}'."
-                next # Saltar esta línea si la fecha no es válida
-              end
-      
-              # Guardar la tarea en el hash
-              @listatareas[nombre] = {
+            nombre, descripcion, estado, creada_en, finalizada_en = linea.chomp.split('|')
+                
+            #Variables para guardar o modificar para la integración al diccionario
+            fecha_creada = Time.strptime(creada_en, "%m/%d/%Y %H:%M:%S") rescue nil
+            fecha_finalizada = finalizada_en.nil? || finalizada_en.empty? ? nil : Time.strptime(finalizada_en, "%m/%d/%Y %H:%M:%S") rescue nil
+            estado_booleano = estado.strip.downcase == "true"
+            
+            #Guardar tareas en un diccionario
+            @listatareas[nombre] = {
                 descripcion: descripcion,
-                estado: estado, # Convertir cadena "true" a booleano
+                estado: estado_booleano,
                 creada_en: fecha_creada,
                 finalizada_en: fecha_finalizada,
-              }
-              puts "Tareas cargadas desde '#{nombre_archivo}'."
-            end
-            
-          else
-            puts "El archivo '#{nombre_archivo}' no existe."
-          end
+            }
+            end #Fin del ciclo do
+            puts ("Tareas cargadas correctamente desde '#{nombre_archivo}'.")
         rescue StandardError => e
-          puts "Hubo un error al intentar cargar las tareas, error: #{e.message}"
-        end
-      end
+            puts("Hubo un error al intentar guardar las tareas, error: #{e.message}")
+        end #Fin del manejo de excepciones
+      end #Fin de la función cargar tareas
+      
       
     
 
 end #Final de la implementación de la clase
 #-----------------------------------------------------------------------------------
-
 
 
 #Funciones principales:
@@ -316,7 +324,7 @@ def main()
             when "5"
                 limpiar_pantalla()
                 puts("\n #{"-" * 15} Guardar tareas en archivo: #{"-" * 15} \n") 
-                tasks.guardar_tarea()
+                tasks.guardar_tareas()
 
             #Fin de Guardar Tareas o Actualizar
 
